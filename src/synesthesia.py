cc9fa19a6ada49eabd48edac91ef20aca8eceb60
gwoho <gwoho@yahoo.com>
2020-05-22 21:28:35 -0700

    Improved re.

--- src/synesthesia.py
+++ src/synesthesia.py
@@ -4,13 +4,13 @@ import re,sys
 
 a = sys.stdin.read()
 
-a = re.sub(r'\b(sn)(?=[\s123468->~()])',r'\\mytweakcolor #red \1',a)
-# a = re.sub(r'\b(hh|hho|hhho|hhc)(?=[\s123468->~()])',r'\\mytweakcolor #yellow \1',a)
-a = re.sub(r'\b(tomh)(?=[\s123468->~()])',r'\\mytweakcolor #yellow \1',a)
-a = re.sub(r'\b(tomml)(?=[\s123468->~()])',r'\\mytweakcolor #blue \1',a)
-a = re.sub(r'\b(cymr)(?=[\s123468->~()])',r'\\mytweakcolor #blue \1',a)
-a = re.sub(r'\b(toml)(?=[\s123468->~()])',r'\\mytweakcolor #green \1',a)
-a = re.sub(r'\b(cymc)(?=[\s123468->~()])',r'\\mytweakcolor #green \1',a)
-a = re.sub(r'\b(bd)(?=[\s123468->~()])',r"\\mytweakcolor #(x11-color 'orange) \1",a)
+a = re.sub(r'\b(sn)(?=[123468_]|\b)',r'\\mytweakcolor #red \1',a)
+# a = re.sub(r'\b(hh|hho|hhho|hhc)(?=[123468_]|\b)',r'\\mytweakcolor #yellow \1',a)
+a = re.sub(r'\b(tomh)(?=[123468_]|\b)',r'\\mytweakcolor #yellow \1',a)
+a = re.sub(r'\b(tomml)(?=[123468_]|\b)',r'\\mytweakcolor #blue \1',a)
+a = re.sub(r'\b(cymr)(?=[123468_]|\b)',r'\\mytweakcolor #blue \1',a)
+a = re.sub(r'\b(toml)(?=[123468_]|\b)',r'\\mytweakcolor #green \1',a)
+a = re.sub(r'\b(cymc)(?=[123468_]|\b)',r'\\mytweakcolor #green \1',a)
+a = re.sub(r'\b(bd)(?=[123468_]|\b)',r"\\mytweakcolor #(x11-color 'orange) \1",a)
 
 sys.stdout.write(a)
