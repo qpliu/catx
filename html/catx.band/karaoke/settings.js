4823f256f6bb6db1befd7f92da773dcf60ed970f
gwoho <gwoho@yahoo.com>
2020-07-20 18:14:02 -0700

    Added sheets to karaoke.  Untransposed Row Row Row Your Boat and Manic Monday.  Simplified build script to get rid of ln -L and that complicated stuff in case ln -L doesn't work.

--- html/catx.band/karaoke/settings.js
+++ html/catx.band/karaoke/settings.js
@@ -3,6 +3,8 @@ class Settings{
 	this.settings = [
 	    ["Delay","delay","200",Number,
 		"Delay in milliseconds to synchronize sound with Jamulus.  Increase if the 球球 bounces too early."],
+	    ["Who","who","kav",String,
+		"Whose sheet music to display.  For example, kav, peter, gg, susan, all."],
 	    ["Microphone Latency","microphoneLatency","150",Number,
 		"Microphone Latency in milliseconds.  Adjust this to line up times of yellow and purple snakes."],
 	    ["Vocal Part","vocalPart","1",Number,
---
+++ html/catx.band/karaoke/sheets.js
@@ -0,0 +1,80 @@
+class Sheets{
+    constructor(where){
+	this.span = document.createElement("span");
+	this.span.onkeydown = (ev)=>this.onkeydown(ev);
+	this.span.style = "position:absolute;left:0;top:0;width:100vw;height:100vh;z-index:-1;";
+	where.appendChild(this.span);
+	this.pages = []
+	document.onkeydown = e=>this.keypress(e);
+    }
+    reset(name){
+	if (name!=this.name){
+	    this.name = name
+	    this.span.innerHTML = "";
+	    this.pages = []
+	    this.page_l = 0;
+	    this.page_r = 1;
+	}
+    }
+    keypress(e){
+	let nextPage;
+	if (e.keyCode==37)
+	    if (this.page_l<this.page_r)
+		nextPage = this.page_r-2;
+	    else
+		nextPage = this.page_l-2;
+	if (e.keyCode==39)
+	    if (this.page_l<this.page_r)
+		nextPage = this.page_l+2;
+	    else
+		nextPage = this.page_r+2;
+	if (nextPage!=undefined && nextPage>=0 && this.pages.length>nextPage && this.pages[nextPage].complete)
+	    if (this.pages[nextPage].naturalWidth==0)
+		this.pages[nextPage].src = this.getPageUrl(nextPage,new Date().getTime());
+	    else if (nextPage&1)
+		this.page_r = nextPage;
+	    else
+		this.page_l = nextPage;
+	this.animate();
+    }
+    getPageUrl(page,time){
+	return encodeURI("/sheet_music/"+this.name+"/"+this.name+"_"+settings.who+".svg"+"?page="+(page+1)+"&t="+time);
+    }
+    animate(){
+	if (!this.enabled)
+	    return;
+	while (this.pages.length<=Math.max(this.page_l,this.page_r)+1){
+	    const page=this.pages.length;
+	    const img=document.createElement("img");
+	    if (page&1)
+		img.style = "display:none;position:absolute;right:0;bottom:0;";
+	    else
+		img.style = "display:none;position:absolute;left:0;bottom:0;";
+	    img.src = this.getPageUrl(page,0);
+	    this.span.appendChild(img);
+	    this.pages[page] = img;
+	}
+	const rect=this.span.getBoundingClientRect();
+	const height=Math.min(rect.height,rect.width*11/8.5/2);
+	const width=height*8.5/11;
+	for (let page=0; page<this.pages.length; page++){
+	    this.pages[page].style.width = width+"px";
+	    this.pages[page].style.height = height+"px";
+	    this.pages[page].style.display = page==this.page_l||page==this.page_r?"block":"none";
+	}
+    }
+    setEnabled(enabled){
+	this.enabled = enabled;
+	this.span.style.display = enabled?"block":"none";
+	const opacity=enabled?.2:1;
+	document.getElementById("name").style.opacity = opacity;
+	document.getElementById("stopImg").style.opacity = opacity;
+	document.getElementById("playImg").style.opacity = opacity;
+	document.getElementById("settingsImg").style.opacity = opacity;
+	document.getElementById("qiuqiusImg").style.opacity = opacity;
+	document.getElementById("snakesImg").style.opacity = opacity;
+	document.getElementById("sheetsImg").style.opacity = opacity;
+	tones.speakerOn.style.opacity = opacity;
+	tones.speakerOff.style.opacity = opacity;
+    }
+}
