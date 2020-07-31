1ade06ba6b68849666a36493769efaef6bfaf607
gwoho <gwoho@yahoo.com>
2020-07-31 14:32:47 -0700

    Now IF stuff can depend on which midi part.

--- build/doif.py
+++ build/doif.py
@@ -4,6 +4,6 @@ import re,sys
 
 for line in sys.stdin:
     m = re.match(r'%\s+IF\s+([^\s]+)\s(.*)$',line)
-    if m and re.match(m.group(1),sys.argv[1]):
+    if m and re.match(m.group(1)+'$',sys.argv[1]):
 	line = m.group(2)
     sys.stdout.write(line)
