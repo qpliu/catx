1af0f3a45eddac9276eb95b8ba4cb00c1eea7a13
gwoho <gwoho@yahoo.com>
2020-07-31 14:15:47 -0700

    Transposed parts of December vocals up an octave for midi part.

---
+++ build/doif.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import re,sys
+
+for line in sys.stdin:
+    m = re.match(r'%\s+IF\s+([^\s]+)\s(.*)$',line)
+    if m and re.match(m.group(1),sys.argv[1]):
+	line = m.group(2)
+    sys.stdout.write(line)
