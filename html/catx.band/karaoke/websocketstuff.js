3fa2ac8e5558505100aaf4bbc1833b181488ddc9
gwoho <gwoho@yahoo.com>
2021-05-24 16:48:56 -0700

    Improve clock synchronization.

--- html/catx.band/karaoke/websocketstuff.js
+++ html/catx.band/karaoke/websocketstuff.js
@@ -1,6 +1,8 @@
 class WebsocketStuff{
     constructor(){
 	this.chatQueue = [];
+	this.ntpArray = [];
+	this.ntpCount = 0;
     }
     do_ntp(){
 	if (--this.timeout_count<0)
@@ -18,17 +20,24 @@ class WebsocketStuff{
     onmessage(event){
 	if (event.data.slice(0,1)=="t"){
 	    if (this.ntp_t0!=undefined){
-		if (this.ntp_offset==undefined)
+		if (this.ntpArray.length==0)
 		    this.websocket.send("middle");
-		this.ntp_offset = (new Date().getTime()+this.ntp_t0)/2-parseInt(event.data.slice(1));
+		this.ntpArray[this.ntpCount] = (new Date().getTime()+this.ntp_t0)/2-parseInt(event.data.slice(1));
+		this.ntpCount = (this.ntpCount+1)%60;
 	    }
 	    this.timeout_count = 4;
 	    while (this.chatQueue.length)
 		this.sendChat(this.chatQueue.shift());
 	}
     }
+    getNtpOffset(){
+	this.ntpArray.sort();
+	return this.ntpArray[this.ntpArray.length>>1];
+    }
     start(){
 	this.ntp_t0 = undefined;
+	this.ntpArray = [];
+	this.ntpCount = 0;
 	this.websocket = new WebSocket("wss://catx.band/karaoke_websocket");
 	this.websocket.onopen = this.onopen.bind(this);
 	this.websocket.onmessage = this.onmessage.bind(this);
