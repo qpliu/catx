69cb1bd75c183500b6d333b6ee4d1a4ee4123dd9
gwoho <gwoho@yahoo.com>
2021-05-24 17:29:17 -0700

    Improved time synchronization.

--- html/catx.band/karaoke/websocketstuff.js
+++ html/catx.band/karaoke/websocketstuff.js
@@ -1,16 +1,13 @@
 class WebsocketStuff{
     constructor(){
 	this.chatQueue = [];
-	this.ntpArray = [];
-	this.ntpCount = 0;
     }
     do_ntp(){
 	if (--this.timeout_count<0)
 	    this.start();
 	else{
-	    this.ntp_t0 = new Date().getTime();
-	    this.websocket.send("ntp");
-	    setTimeout(this.do_ntp.bind(this),1000);
+	    this.websocket.send("ntp"+new Date().getTime());
+	    setTimeout(this.do_ntp.bind(this),250);
 	}
     }
     onopen(){
@@ -19,12 +16,11 @@ class WebsocketStuff{
     }
     onmessage(event){
 	if (event.data.slice(0,1)=="t"){
-	    if (this.ntp_t0!=undefined){
-		if (this.ntpArray.length==0)
-		    this.websocket.send("middle");
-		this.ntpArray[this.ntpCount] = (new Date().getTime()+this.ntp_t0)/2-parseInt(event.data.slice(1));
-		this.ntpCount = (this.ntpCount+1)%60;
-	    }
+	    if (this.ntpArray.length==4)
+		this.websocket.send("middle");
+	    const a=event.data.slice(1).split(",");
+	    this.ntpArray[this.ntpCount] = .5*(new Date().getTime()+parseInt(a[0]))-parseInt(a[1]);
+	    this.ntpCount = (this.ntpCount+1)%60;
 	    this.timeout_count = 4;
 	    while (this.chatQueue.length)
 		this.sendChat(this.chatQueue.shift());
@@ -35,7 +31,6 @@ class WebsocketStuff{
 	return this.ntpArray[this.ntpArray.length>>1];
     }
     start(){
-	this.ntp_t0 = undefined;
 	this.ntpArray = [];
 	this.ntpCount = 0;
 	this.websocket = new WebSocket("wss://catx.band/karaoke_websocket");
